8541cf918003fb04872da67a71623962979e73fc
Sudhanshu <heyitsmemomot@gmail.com>
2022-04-18 22:27:56 +0530

    main file formatting

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ import time
 
 console = Console()
 
-reddit = praw.Reddit(client_id = ids.CLIENT_ID, client_secret = ids.SECRET_KEY, username= 'Dangerous-Hornet9878', password= ids.PASS, user_agent= 'SandeshVahak')
+reddit = praw.Reddit(
+    client_id=ids.CLIENT_ID,
+    client_secret=ids.SECRET_KEY,
+    username="Dangerous-Hornet9878",
+    password=ids.PASS,
+    user_agent="SandeshVahak",
+)
 
-msg = '''Hi
+msg = """Hi
 
 I am very impressed by your profile here. You seems like a great person. I like the way you roll.
 
 Can you please tell me what is your username in bakchodi[dot]org website?? I wanted to follow you up there as I am detoxing from foreign platforms and moving to Indian ones. I don't want to miss your posts and thoughts.
 
-Thank you very much sir. '''
+Thank you very much sir. """
 
 
-def revisitcheck(usrnm):
+def revisit_check(usrnm):
     username_exists = False
-    with open('sent.csv', 'r') as userchecker:
+    with open("sent.csv", "r") as userchecker:
         csv_reader2 = csv.reader(userchecker)
         for line in csv_reader2:
             try:
@@ -31,29 +37,29 @@ def revisitcheck(usrnm):
     return username_exists
 
 
-def visitwrite(usrnm):
-    with open("sent.csv", "a") as usrlist:
-        usrlist.write(usrnm + "\n")
+def visit_write(usrnm):
+    with open("sent.csv", "a") as usr_list:
+        usr_list.write(usrnm + "\n")
 
 
 def main():
-    with open('tobesent.csv', 'r') as usergetter:
-        csv_reader = csv.reader(usergetter)
+    with open("tobesent.csv", "r") as user_getter:
+        csv_reader = csv.reader(user_getter)
 
         for line in csv_reader:
-            with Halo(text='...', spinner='dots'):
+            with Halo(text="...", spinner="dots"):
                 raw_line = line[0]
                 list_raw_line = list(map(str, raw_line.split(" ")))
                 username = list_raw_line[0]
-                if not revisitcheck(username):
+                if not revisit_check(username):
                     try:
                         reddit.redditor(username).message(subject="Invite", message=msg)
-                        visitwrite(username)
+                        visit_write(username)
                         console.log(f"Sent invite to {username}.", style="bold green")
                         time.sleep(30)
                     except:
                         pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
